8f5d25c3ad31cf701844fc43c4bcda99ddba43a6
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-15 17:22:01 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MyApp() );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(                // Material widget is the root of my application.
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: BoilerPlate(),
+      debugShowCheckedModeBanner: false,    // It disable the red banner strip at left top
+    );
+  }
+}
+
+// BoilerPlate class
+class BoilerPlate extends StatelessWidget {
+  const BoilerPlate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Boiler Plate"),
+        backgroundColor: Colors.red.shade500,
+      ),
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+          child: Center(
+              child: Text("Hello Ratndeep...",),
+          ),
+      ),
+    );
+  }
+}
+
+
+ 

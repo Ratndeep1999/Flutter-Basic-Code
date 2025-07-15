59fdba9fa967e04a2af0d546d467cffb0cfd033d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-15 17:36:16 +0530

    main.dart : entry point main(),

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,8 @@
 
 
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp( const MyApp() );
+
+
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(                // Material widget is the root of my application.
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: BoilerPlate(),
-      debugShowCheckedModeBanner: false,    // It disable the red banner strip at left top
-    );
-  }
-}
-
-// BoilerPlate class
-class BoilerPlate extends StatelessWidget {
-  const BoilerPlate({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Boiler Plate"),
-        backgroundColor: Colors.red.shade500,
-      ),
-      backgroundColor: Colors.grey,
-      body: SafeArea(
-          child: Center(
-              child: Text("Hello Ratndeep...",),
-          ),
-      ),
-    );
-  }
-}
-
-
- 
